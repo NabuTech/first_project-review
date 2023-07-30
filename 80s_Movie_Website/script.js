2d9ca17d340ee1b86e1e09b708ffc2d0e6111cce
NabuTech <nabutech@proton.me>
2023-07-31 09:29:15 +1200

    Embedded Videos

--- 80s_Movie_Website/script.js
+++ 80s_Movie_Website/script.js
@@ -1,14 +1,40 @@
-function toggleVideo(cardId) {
-    const iframe = document.getElementById(cardId);
 
-    if (!iframe.src || iframe.src === "") {
-      const videoId = cardId.substring(5);
-      iframe.src = `https://www.youtube.com/embed/${videoId}`;
-      iframe.style.display = "block";
-      iframe.previousElementSibling.style.display = "none";
-    } else {
-      iframe.src = "";
-      iframe.style.display = "none";
-      iframe.previousElementSibling.style.display = "block";
-    }
-  }z
+
+function playVideo(videoId) {
+  const movieElement = document.getElementById(videoId);
+
+  const videoUrl = getVideoUrlForMovie(videoId);
+
+  const iframeElement = document.createElement('iframe');
+  iframeElement.src = videoUrl;
+  iframeElement.width = "100%";
+  iframeElement.height = "100%";
+  iframeElement.title = "YouTube video player";
+  iframeElement.frameborder = "0";
+  iframeElement.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+  iframeElement.allowfullscreen = true;
+
+  movieElement.classList.add('video-playing'); // Add a class to indicate video is playing
+  movieElement.innerHTML = '';
+  movieElement.appendChild(iframeElement);
+}
+
+function getVideoUrlForMovie(videoId) {
+  // Mapping of video IDs to their corresponding YouTube URLs
+  const videoIdToUrlMap = {
+    "EmpireStrikesBack": "https://www.youtube.com/embed/JNwNXF9Y6kY?autoplay=1",
+    "Raiders": "https://www.youtube.com/embed/0xQSIdSRlAk?autoplay=1",
+    "ET": "https://www.youtube.com/embed/qYAETtIIClk?autoplay=1",
+    "ReturnoftheJedi":"https://www.youtube.com/embed/7L8p7_SLzvU?autoplay=1",
+    "BeverlyHillsCop":"https://www.youtube.com/embed/oc3sPICXZLs?autoplay=1",
+    "BackToTheFuture":"https://www.youtube.com/embed/qvsgGtivCgs?autoplay=1",
+    "TopGun":"https://www.youtube.com/embed/xa_z57UatDY?autoplay=1",
+    "ThreeMenandaBaby":"https://www.youtube.com/embed/ohBNZZKmtnw?autoplay=1",
+    "RainMan":"https://www.youtube.com/embed/mlNwXuHUA8I?autoplay=1",
+    "IndianaJonesLastCrusade":"https://www.youtube.com/embed/DKg36LBVgfg?autoplay=1" ,
+    
+  };
+
+  return videoIdToUrlMap[videoId];
+}
+
